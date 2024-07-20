19be7082a8a6bd489ce793bfed51452ef85f7e8f
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-07-20 23:49:40 +0800

    successfull test of fetch API

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+//create object for base link and API key
+const weatherAPI = {
+    "key": "GD5M8NUS9XCWKJ7TVPY4MFD8A",
+    "url": "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/",
+};
+const urlExtension1 = "?unitGroup=us&include=days%2Calerts%2Ccurrent&key=";
+const urlExtension2 = "&contentType=json";
+
 const inputPlace = document.querySelector(".main-form-input");
 const inputForm = document.querySelector("#main-form");
 
@@ -20,10 +28,27 @@ inputForm.addEventListener('submit', function (e) {
     } else {
         inputPlace.setCustomValidity("");
     }
-    console.log(inputPlace.value);
+    const location = inputPlace.value;
+    console.log(location);
+    getRawWeatherData(location);
 });
-
-//create object for base link and API key
-const weatherAPI = {
-    
+//get weather data
+async function getRawWeatherData(location) {
+    try {
+        const response = await fetch(`${weatherAPI.url}${location}${urlExtension1}${weatherAPI.key}${urlExtension2}`, {mode: 'cors'});
+        
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            //Create an error function to log status from HTTP response
+        } else {
+            const parseResponse = await response.json().then(response => {
+                /* need to use .then function to process the response instead of only pending
+                in this function you must also parse the JSON and equate it to what data are needed*/
+                console.log(response);
+                //continue here
+            });
+        }
+    } catch (error) {//use catch to furher process the error
+        console.log(`Error: ${error.message}`);
+    }
 }
